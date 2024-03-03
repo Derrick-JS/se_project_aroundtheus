d6ecd901b1aaabd28138a4c68aa0a93cb5292858
Derrick-JS <derrickjsanchez458@gmail.com>
2024-03-03 06:05:52 -0800

    Implement ability to open and close modal to edit profile

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,38 @@
-const initialCards = [ 
-    {
-        name: 'Yosemite Valley',
-        link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg'
-    },
-    {
-        name: 'Lake Louise',
-        link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg'
-    },
-    {
-        name: 'Bald Mountains',
-        link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg'
-    },
-    {
-        name: 'Latemar',
-        link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg'
-    },
-    {
-        name: 'Vanoise National Park',
-        link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg'
-    },
-    {
-        name: 'Lago di Braies',
-        link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg'
-    }
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+  },
+  {
+    name: "Lake Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+  },
 ];
+
+let profileEditButton = document.querySelector(".profile__edit-button");
+let modal = document.querySelector(".modal");
+/* should future-proof by changing from modal in cases where we have multiple modals */
+let modalCloseButton = document.querySelector(".modal__button-close");
+
+profileEditButton.addEventListener("click", function () {
+  modal.classList.add("modal__open");
+});
+modalCloseButton.addEventListener("click", function () {
+  modal.classList.remove("modal__open");
+});
